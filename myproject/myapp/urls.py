b3f0a636fe41955bbf0f1bd1d22dabf4d6583beb
NiyaRai13 <niyarai2963@gmail.com>
2025-07-10 13:53:54 +0545

    blogs

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_views import login,register,logout_page
-from .views.main_views import index,addblog,blog_page
+from .views.main_views import index,addblog,blog_page,edit_blog
 
 urlpatterns=[
     path('login/',login, name='login'),
@@ -9,4 +9,5 @@ urlpatterns=[
     path('addblog/',addblog,name='addblog'),
     path('', index, name='index'),
     path('blog/<int:id>', blog_page, name='blog_page'),
+    path('editblog/<int:id>', edit_blog, name='edit_blog'),
 ]
--- myproject/myapp/views/main_views.py
+++ myproject/myapp/views/main_views.py
@@ -54,3 +54,49 @@ def addblog(request):
 def blog_page(request,id):
      blog=Blog.objects.get(id=id)
      return render(request,'main/blogpage.html',{'blog':blog})
+
+def edit_blog(request,id):
+    blog=Blog.objects.get(id=id)
+    errors={}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        image = request.FILES.get('image')
+        category = request.POST.get('category')
+        description = request.POST.get('description')
+
+        if blog.author != request.user:
+            return render(request, 'main/editblog.html', {'error': 'You are not authorized to edit this blog.'})
+
+        if not title:
+            errors['title']="Title is required"
+
+        
+        if not category:
+             errors['category']="Category is required"
+
+        if not description:
+                errors['description']="Description is required"
+
+        if not image:
+                errors['image']="Image is required"
+   
+        if errors:
+              return render (request,'main/editblog.html',{'errors':errors,'data':request.POST,'blog':blog})
+         
+             
+        blog.title=title
+        blog.category=category
+        blog.image=image
+        blog.description=description
+        blog.save()
+
+        return render(request, 'main/editblog.html', {'message': 'Blog updated successfully!','blog':blog})
+
+    return render(request, 'main/editblog.html',{'blog':blog})
+
+def delete_blog(request,id):
+    blog=Blog.objects.get(id=id)
+    if request.method == 'POST':
+        blog.delete()
+        return render(request, 'main/index.html', {'message': 'Blog deleted successfully!'})
+    return render(request, 'main/deleteblog.html', {'blog': blog})
